8b7f5a4092689fa285fb80fe0923b714ff5de0fe
yuri_sn <yuri.snitko@gmail.com>
2023-07-19 20:55:54 +0300

    add components

---
+++ br2jsx/components/BlockComponent.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import RainBowFrame from './RainBowFrame';
+
+const BlockComponent = () => {
+  let colors = ['red', 'orange', 'yellow', 'green', '#00BFF', 'blue', 'purple'];
+  return <RainBowFrame colors={colors}>hello!</RainBowFrame>;
+};
+
+export default BlockComponent;
---
+++ br2jsx/components/RainBowFrame.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+const RainbowFrame = (props) => {
+  const { colors } = props;
+  if (colors.length === 0) {
+    return <div>{props.children}</div>;
+  }
+  return (
+    <div
+      style={{
+        border: `2px solid ${colors[0]}`,
+        padding: '5px',
+        textAlign: 'center',
+      }}
+    >
+      <RainbowFrame colors={colors.slice(1)}>{props.children}</RainbowFrame>
+    </div>
+  );
+};
+
+export default RainbowFrame;
