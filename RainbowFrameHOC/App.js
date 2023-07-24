d7eec82d497e4077b0cbfd61394e150d15cd7762
yuri_sn <yuri.snitko@gmail.com>
2023-07-24 20:58:41 +0300

    add new project

---
+++ RainbowFrameHOC/App.js
@@ -0,0 +1,6 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import BlockComponent from "./components/BlockComponent";
+
+ReactDOM.render(<BlockComponent />, document.getElementById("container"));
